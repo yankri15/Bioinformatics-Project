738b2a55df5fa2ac66e771190eb47efe5ade1f7a
YaakovHatam <6986221+YaakovHatam@users.noreply.github.com>
2024-03-21 09:33:34 +0200

    remove cache from git

--- partc.py
+++ partc.py
@@ -1,3 +1,34 @@
 from Bio import SeqIO
+import pandas as pd
+import matplotlib.pyplot as plt
+import Bio.Data.CodonTable
+import seaborn as sns
 
-print(Bio.data.CodonTable.standart_dna_table)
+# 1. בחלק ג' בפרויקט הסופי אפשר להשתמש בפונקציה המובנת עבור החישוב של dnds.
+
+
+# 1. Get the standard_dna_table
+standard_table = Bio.Data.CodonTable.standard_dna_table
+print(standard_table)
+
+# 2. Get the codons
+codons = standard_table.forward_table
+print(codons)
+
+
+# 3. group by amino acid
+aa_codons = {}
+for codon, aa in codons.items():
+    if aa not in aa_codons:
+        aa_codons[aa] = []
+    aa_codons[aa].append(codon)
+
+print(aa_codons)
+
+synonimous_codons = {}
+
+for key, strings in aa_codons.items():
+    for string in strings:
+        if key not in synonimous_codons:
+            synonimous_codons[key] = []
+        synonimous_codons[key].append(string)
